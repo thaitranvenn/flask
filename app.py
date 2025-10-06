d1043d891e0b190df82e7ae40370879e49efd25b
thaitranvenn <thaitran130399.tusur@gmail.com>
2025-10-06 12:24:37 +0700

    Initial flask api project

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api/add", methods=["GET"])
+def add_numbers():
+    # Lấy tham số từ query string
+    a = request.args.get("a", type=float)
+    b = request.args.get("b", type=float)
+
+    if a is None or b is None:
+        return jsonify({"error": "Not enough argument"}), 400
+
+    result = a + b
+    return jsonify({"a": a, "b": b, "result": result})
+
+if __name__ == "__main__":
+    app.run(debug=True)
